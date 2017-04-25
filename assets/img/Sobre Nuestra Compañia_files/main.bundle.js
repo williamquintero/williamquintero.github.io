c0f122be3d8c2f9f909e6e91db447c802ba947de
Cristian Dorado <cdorado330@gmail.com>
2017-04-25 16:28:25 -0500

    Comió inicial

---
+++ assets/img/Sobre Nuestra Compañia_files/main.bundle.js
@@ -0,0 +1,264 @@
+webpackJsonp([1,4],{
+
+/***/ 347:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return HomePage; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var HomePage = (function () {
+    function HomePage() {
+    }
+    HomePage = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_6" /* Component */])({
+            selector: 'page-home',
+            template: __webpack_require__(706),
+            styles: [__webpack_require__(704)]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], HomePage);
+    return HomePage;
+}());
+//# sourceMappingURL=/Users/chrdorad/Documents/Proyectos/seguros/WQSeguros/src/home.component.js.map
+
+/***/ }),
+
+/***/ 416:
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = 416;
+
+
+/***/ }),
+
+/***/ 417:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__ = __webpack_require__(506);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__environments_environment__ = __webpack_require__(541);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__app_app_module__ = __webpack_require__(538);
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_2__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_3__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=/Users/chrdorad/Documents/Proyectos/seguros/WQSeguros/src/main.js.map
+
+/***/ }),
+
+/***/ 537:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'app works!';
+    }
+    AppComponent = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_6" /* Component */])({
+            selector: 'my-app',
+            template: __webpack_require__(705),
+            styles: [__webpack_require__(703)]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+//# sourceMappingURL=/Users/chrdorad/Documents/Proyectos/seguros/WQSeguros/src/app.component.js.map
+
+/***/ }),
+
+/***/ 538:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(159);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__(496);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__(502);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__(537);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__init_component__ = __webpack_require__(539);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pages_home_home_component__ = __webpack_require__(347);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__angular_router__ = __webpack_require__(526);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__routes_routes_component__ = __webpack_require__(540);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */],
+                __WEBPACK_IMPORTED_MODULE_5__init_component__["a" /* InitComponent */],
+                __WEBPACK_IMPORTED_MODULE_6__pages_home_home_component__["a" /* HomePage */]
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+                __WEBPACK_IMPORTED_MODULE_3__angular_http__["a" /* HttpModule */],
+                __WEBPACK_IMPORTED_MODULE_7__angular_router__["a" /* RouterModule */].forRoot(__WEBPACK_IMPORTED_MODULE_8__routes_routes_component__["a" /* APPROUTER */])
+            ],
+            providers: [],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_5__init_component__["a" /* InitComponent */]]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppModule);
+    return AppModule;
+}());
+//# sourceMappingURL=/Users/chrdorad/Documents/Proyectos/seguros/WQSeguros/src/app.module.js.map
+
+/***/ }),
+
+/***/ 539:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return InitComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var InitComponent = (function () {
+    function InitComponent() {
+    }
+    InitComponent = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_6" /* Component */])({
+            selector: 'init-component',
+            template: '<router-outlet></router-outlet>'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], InitComponent);
+    return InitComponent;
+}());
+//# sourceMappingURL=/Users/chrdorad/Documents/Proyectos/seguros/WQSeguros/src/init.component.js.map
+
+/***/ }),
+
+/***/ 540:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__pages_home_home_component__ = __webpack_require__(347);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return APPROUTER; });
+
+var APPROUTER = [
+    { path: '', component: __WEBPACK_IMPORTED_MODULE_0__pages_home_home_component__["a" /* HomePage */] },
+];
+//# sourceMappingURL=/Users/chrdorad/Documents/Proyectos/seguros/WQSeguros/src/routes.component.js.map
+
+/***/ }),
+
+/***/ 541:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=/Users/chrdorad/Documents/Proyectos/seguros/WQSeguros/src/environment.js.map
+
+/***/ }),
+
+/***/ 703:
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ 704:
+/***/ (function(module, exports) {
+
+module.exports = "body{\n    font-family: 'Montserrat', sans-serif;\n    margin: 0;\n}\n.logo{\n    position: relative;\n}\n.logo figure{\n display: inline-block;\n}\n.logo ul{\n\n    display: inline-block;\n    position: absolute;\n    right: 0;\n    margin-top: 45px;\n    margin-right: 45px;\n\n}\n.logo li{\n\n    display: inline-block;\n    margin-left: 10px;\n    \n    \n}\n.portada{ \n\n    background-image: url('../../../assets/img/portada.png');\n    background-repeat: no-repeat;\n    height: 500px;\n    width: 100%\n    \n\n}\n.portada figure{\n    margin-bottom: 0;\n}\n.header{\n    background: #004F6F;    \n    height: 80px;\n    \n    \n}\n.header ul{\n    margin-top: 0;\n}\n.header li{\n    margin-top: 25px;\n}\n.menu{\n    font-size: 24px;    \n    color: white;    \n}\n.menu a{\n    padding-right: 30px;\n}\n\n.menu li{\n\n    display: inline-block;\n    \n}\n.mision{\n  display: inline-block;\n  background-image: url('../../../assets/img/mision.png');\n  height: 400px;\n  width: 49%;\n  border-radius: 10px;\n  \n   \n}\n.vision{\n    display: inline-block;\n    background-image: url('../../../assets/img/vision.png');\n    height: 400px;\n    width: 49%;\n    float:right;\n    border-radius: 10px;\n    \n    \n}\n.contenedor-mision{\n\n    height: 400px;\n    margin: 25px;\n}\n.compra{\n    background: #004F6F;\n    border-radius: 10px;\n}\n.compra h1{\n    color: #F4A859;\n    font-size: 100px;\n}\n.compra h3{\n    color: white;\n}\n.compra h2{\n    color: white;\n    display: inline;\n}\n\n.footer{\n    background: #004F6F;\n    height: 200px;\n    color: white;\n}\n.footer ul{\n\n    display: inline-block;\n    position: absolute;\n    right: 350px;\n    margin-top: 45px;\n    margin-right: 45px;\n\n}\n.footer li{\n\n    display: inline-block;\n    margin-left: 10px;\n    \n    \n}\n.footer-follow{\n    font-size: 50px;\n    position: absolute;\n    left: 280px;\n    margin-top: 50px;\n    font-weight: bold;        \n}\n\n.footer ul{\n    display: block;\n} \n\n"
+
+/***/ }),
+
+/***/ 705:
+/***/ (function(module, exports) {
+
+module.exports = "<h1>\n  {{title}}\n</h1>\n"
+
+/***/ }),
+
+/***/ 706:
+/***/ (function(module, exports) {
+
+module.exports = "<head>\n    <title>Sobre Nuestra Compañia</title>\n    <link href=\"https://fonts.googleapis.com/css?family=Montserrat\" rel=\"stylesheet\">\n</head>\n<body>\n    <section class=\"logo\">\n        <figure>\n            <img src = \"../../../assets/img/logo.png\" alt=\"logotipo\"/>\n        </figure>\n        <ul>\n            <li>\n                <a href=\"https://www.facebook.com/\" target=\"_blank\"><img src = \"../../../assets/img/facebook.png\" width=\"50\" height=\"50\"/></a>\n            </li>\n            <li>\n                <a href=\"https://twitter.com/?lang=es\" target=\"_blank\"><img src = \"../../../assets/img/twitter.png\" width=\"50\" height=\"50\"/></a>\n            </li>\n            <li>\n                <a href=\"https://www.instagram.com/\" target=\"_blank\"><img src = \"../../../assets/img/instagram.png\" width=\"50\" height=\"50\"/></a>\n            </li>\n        </ul>\n    </section>\n    <section id=\"portada\" class=\"portada\">\n        <figure></figure>\n    </section>\n    <header id=\"header\" class=\"header\">                \n        <nav class= \"menu\">\n            <ul>\n                <li><a>Nosotros</a></li>\n                <li><a>Partners</a></li>\n                <li><a>Seguros</a></li>\n                <li><a>SOAT</a></li>\n                <li><a>Seguro Para Autos</a></li>\n                <li><a>Contactenos</a></li>\n            </ul>\n        </nav>\n    </header>\n    <div class=\"contenedor-mision\">\n        <section id=\"mision\" class=\"mision\">\n            <h2>Mision</h2>\n        </section>\n        <section id=\"vision\" class=\"vision\">\n            <h2>Vision</h2>\n        </section>\n    </div>\n    <section id=\"compra\" class=\"compra\">\n        <h2>Compra tu</h2>\n        <h1>SOAT</h1>\n        <h3>Facil, rapido y paga contraentrega en la puerta de tu hogar</h3>\n        <button>Ingresa aqui</button>\n    </section>\n    <section id=\"seguroautos\" class=\"seguroautos\">\n        <article>\n            <h3>¿QUE ES UN SEGURO DE AUTOMOVILES?</h3>\n            <p>Un Seguro de Automóviles es un contrato voluntario mediante el cual el propietario de un vehículo traslada a una \n               compañía de seguros los riesgos asociados a la conducción de un automotor, como los daños por accidentes de tránsito, \n               el hurto del carro, los daños ocasionados a bienes de terceros y la muerte o lesiones ocasionados a terceros como \n               consecuencia de un accidente de transito en donde el asegurado resulte responsable.</p>\n        </article>\n    </section>\n    <section id=\"renovar\" class=\"renovar\">\n        <h2>RECUERDA RENOVAR EL SEGURO OBLIGATORIO - SOAT</h2>\n        <h3>A Domicilio! asi de facil y seguro </h3>\n    </section>\n    <footer id = \"footer\" class=\"footer\">\n           \n        <span class=\"footer-follow\">Siguenos en:</span>        \n        <ul>\n            <li>\n                <a href=\"https://www.facebook.com/\" target=\"_blank\"><img src = \"../../../assets/img/facebook.png\" width=\"80\" height=\"80\"/></a>\n            </li>\n            <li>\n                <a href=\"https://twitter.com/?lang=es\" target=\"_blank\"><img src = \"../../../assets/img/twitter.png\" width=\"80\" height=\"80\"/></a>\n            </li>\n            <li>\n                <a href=\"https://www.instagram.com/\" target=\"_blank\"><img src = \"../../../assets/img/instagram.png\" width=\"80\" height=\"80\"/></a>\n            </li>\n        </ul>\n      \n      <div class = \"footer-mail\">\n            <span>Para resolver dudas, comentarios y sugerencias, Comuníquese a nuestros numeros 6363778 / 7033604. Si quiere mayor información puede acércarce \n                a nuestras oficinas en Bogotá. Carrera 14 #79-78 oficina 404.</span>\n            <a href=\"mailto:wquinteroseguros@gmail.com\"><strong>E-mail: </strong><span>wquinteroseguros@gmail.com</span></a>            \n      </div>      \n    </footer>\n</body>\n"
+
+/***/ }),
+
+/***/ 760:
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(420);
+module.exports = __webpack_require__(417);
+
+
+/***/ })
+
+},[760]);
+//# sourceMappingURL=main.bundle.map
