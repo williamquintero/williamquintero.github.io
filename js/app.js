1c815afe45cc468cbdf4ca2272843c3de15f1f9a
Cristian Dorado <cdoradodev@gmail.com>
2017-05-10 00:42:50 -0500

    se agrego responsive

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation()
+
+const $offCanvas = $('#offCanvas')
+const $sticky = $('#sticky')
+$offCanvas.find('li').click(function (ev){
+
+    $offCanvas.foundation('close')
+    setTimeout(function(){
+        $sticky.css('left','0px')
+    },300)
+})
